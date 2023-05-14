5e6d25b513e3a418ffea6a7dbc2d1493862eebc9
pyother <piotr.dominik.sobol@gmail.com>
2023-05-14 20:40:02 +0000

    Add RestaurantBase ERD diagram

--- apps/artbase/app/models.py
+++ apps/artbase/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    artist_name = models.CharField('Artist Name', max_length=30)
+
+class Artwork(models.Model):
+    pass
+
+class GroupOfArtworks(models.Model):
+    pass
+
+class Customer(models.Model):
+    pass
+
+class Transactions(models.Model):
+    pass
