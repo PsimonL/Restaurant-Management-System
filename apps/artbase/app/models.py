fdb6cd6313847253e633c1feba419fd524fa0841
pyother <piotr.dominik.sobol@gmail.com>
2023-05-16 13:40:52 +0000

    Create database

--- apps/artbase/app/models.py
+++ apps/artbase/app/models.py
@@ -1,16 +1,13 @@
 from django.db import models
 
-class Artist(models.Model):
-    artist_name = models.CharField('Artist Name', max_length=30)
-
-class Artwork(models.Model):
-    pass
-
-class GroupOfArtworks(models.Model):
-    pass
-
 class Customer(models.Model):
-    pass
+    customer_name = models.CharField('Customer Full Name: ', max_length=30)
+    customer_spendings = models.IntegerField('Customer Spendings: ')
+    customer_premium = models.BooleanField('Customer Premium: ', max_length=30, default=0)
 
-class Transactions(models.Model):
-    pass
+class Order(models.Model):
+    order_type = models.CharField('Order Type: ', max_length=30, default="")
+
+class Catalog(models.Model):
+    catalog_dish = models.CharField('Dish Name: ', max_length=30)
+    catalog_price = models.IntegerField('Dish Price: ')
