161bc06c0d87335b50fc539bcaf5c901bbe7a901
Szymon Rogowski <szymon0r0official@gmail.com>
2023-05-29 00:12:50 +0200

    Fixing models.py

--- apps/restaurant-base/app/models.py
+++ apps/restaurant-base/app/models.py
@@ -2,25 +2,20 @@ from django.db import models
 
 
 class Customer(models.Model):
-    customer_name = models.CharField('Customer Full Name', max_length=50, default="Customer name not entered!",
-                                     blank=False, null=False)
+    customer_name = models.CharField('Customer Full Name', max_length=50, blank=False, null=False)
     customer_status = models.BooleanField('Customer Status', default=False, blank=False, null=False)
-    customer_address = models.CharField('Customer Address', max_length=60, default="Customer address name not entered!",
-                                        blank=False, null=False)
+    customer_address = models.CharField('Customer Address', max_length=60, blank=False, null=False)
     customer_contact = models.IntegerField('Customer Contact Number', default=0, blank=False, null=False)
 
 
 class Catalog(models.Model):
-    catalog_dish = models.CharField('Dish Name', max_length=30, default="Dish name not entered!",
-                                    blank=False, null=False)
+    catalog_dish = models.CharField('Dish Name', max_length=30, blank=False, null=False)
     catalog_price = models.IntegerField('Dish Price', default=0, blank=False, null=False)
-    catalog_description = models.CharField('Dish Description', max_length=100, default="Description not entered!",
-                                           blank=False, null=False)
+    catalog_description = models.CharField('Dish Description', max_length=100, blank=False, null=False)
 
 
 class Employee(models.Model):
-    employee_name = models.CharField("Employee Name", max_length=50, default="Employee name not entered!",
-                                     blank=False, null=False)
+    employee_name = models.CharField("Employee Name", max_length=50, blank=False, null=False)
 
 
 class Order(models.Model):
